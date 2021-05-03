50694df5318b7d0f74cf348fe3921427d884cba0
0ojixueseno0 <jz0ojiang@qq.com>
2021-05-03 20:19:24 +0800

    player不动

--- App.py
+++ App.py
@@ -65,7 +65,15 @@ class Assets(object):
       pygame.transform.scale(pygame.image.load('assets/picture/yes.png').convert_alpha(),(45,45)),
       pygame.transform.scale(pygame.image.load('assets/picture/no.png').convert_alpha(),(45,45)),
       pygame.image.load('assets/picture/heart.png').convert_alpha(),
-      pygame.image.load('assets/picture/heart_half.png').convert_alpha()
+      pygame.image.load('assets/picture/heart_half.png').convert_alpha(),
+      #* 障碍物
+      pygame.image.load('assets/picture/cloud.png').convert_alpha(),
+      pygame.image.load('assets/picture/airplane.png').convert_alpha(),
+      pygame.image.load('assets/picture/skyisland.png').convert_alpha(),
+      pygame.image.load('assets/picture/stree.png').convert_alpha(),
+      pygame.image.load('assets/picture/ttree.png').convert_alpha(),
+      pygame.image.load('assets/picture/built.png').convert_alpha()
+
     )
     #音效
     
@@ -73,6 +81,7 @@ class Assets(object):
     self.sounds["jump"] = pygame.mixer.Sound('assets/sounds/jump.wav')
     self.sounds["gameover"] = pygame.mixer.Sound('assets/sounds/gameover.mp3')
     self.sounds["switch"] = pygame.mixer.Sound('assets/sounds/switch.mp3')
+    self.sounds["pickheart"] = pygame.mixer.Sound('assets/sounds/pickheart.ogg')
 
 # 配置文件
 class Config(object):
@@ -91,11 +100,77 @@ class Config(object):
     with open("./config.json",'w',encoding='utf-8') as f:
       f.write(json.dumps(self.config,indent=4,ensure_ascii=False))
 
+# 精灵
+class GSprite(pygame.sprite.Sprite):
+  def __init__(self, image, speed=1):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = image
+    self.rect = self.image.get_rect()
+    self.speed = speed
+
+  # #X 属性
+  # def _getx(self): return self.rect.x
+  # def _setx(self,value): self.rect.x = value
+  # X = property(_getx,_setx)
+
+  # #Y 属性
+  # def _gety(self): return self.rect.y
+  # def _sety(self,value): self.rect.y = value
+  # Y = property(_gety,_sety)
+
+  #position 属性
+  def _getpos(self): return self.rect.topleft
+  def _setpos(self,pos): self.rect.topleft = pos
+  position = property(_getpos,_setpos)
+
 class Player(object):
   def __init__(self,screen):
     self.screen = screen
-    self.score = 0
-    self.health = 9
+    #* 玩家变量
+    self.score = 0                      # 初始得分
+    self.health = 9                     # 初始血量
+    self.onground = False               # 是否触地
+    self.gravity = 0.5                  # 重力
+    self.friction = 1                   # 摩擦力
+    self.speed = 1                      # 重力速度
+    self.hurttime = self.screen.fps * 3 # 受伤后的无敌时间(s)
+    
+    # 精灵组
+    self.player_group = pygame.sprite.Group()
+    # 玩家初始化
+    self.player = GSprite(screen.assets.images["ELEMENTS"][0], speed = self.speed)
+    self.player.position = (400,800)
+    self.player_group.add(self.player)
+
+
+  #TODO: voice
+  def Jump(self):
+    print("Jump")
+    if self.onground:
+      self.onground = False
+      self.speed = -32 * self.gravity
+  
+  def Switch(self):
+    print("Switch")
+    if self.onground:
+      self.onground = False
+      self.gravity *= -1
+
+  # 重力 摩擦力计算
+  def Action(self):
+    self.speed *= self.friction # 空气摩擦
+    self.speed += self.gravity # 重力加速度
+    self.player.rect.y = self.speed
+    if self.player.rect.top < 100 or self.player.rect.bottom > self.screen.height - 100:
+      if abs(self.speed) > 4:
+        self.speed *= -0.3
+      else:
+        self.speed = 0
+    self.player.rect.top = min(max(self.player.rect.top,100),self.screen.height-100)
+    self.player.rect.bottom = min(max(self.player.rect.bottom,100), self.screen.height-100)
+    if self.speed == 0:
+      self.onground = True
+
 
 # 点击事件判断
 class ClickEvent(object):
@@ -155,10 +230,13 @@ class MainAction(object):
   def __init__(self, screen):
     self.screen = screen
     
-    #TODO: rollbg
   def Actions(self):
     if self.screen.mainDraw.uipart == 2:
-      self.screen.roolBG.action()
+      self.screen.roolBG.action() # 背景滚动
+      
+      #* 玩家数值
+      self.screen.player.Action()
+      
     pass
 
 # 主要绘制
@@ -223,13 +301,15 @@ class MainDraw(object):
       self.screen.scene.blit(self.screen.assets.images["UI"][2], (0,0))
       self.drawheart()
       self.drawscore()
+      # self.screen.player.player_group.draw(self.screen.scene)
+      self.screen.scene.blit(self.screen.player.player.image, self.screen.player.player.rect)
 
 # 主场景
 class MainScene(object):
   # 初始化主场景
   def __init__(self):
     # 游戏尺寸
-    self.size = (1600,900)
+    self.size = self.width, self.height = (1600,900)
     # 场景对象
     self.scene = pygame.display.set_mode([self.size[0], self.size[1]])
 
@@ -239,19 +319,24 @@ class MainScene(object):
     # 窗口logo
     pygame.display.set_icon(pygame.image.load('assets/logo.png').convert_alpha())
 
+    # 帧率
     self.fps = 120
     self.fclock = pygame.time.Clock()
+
+    # 调试模式
+    self.debug = False
 
     # pygame.mixer.init()
 
     #* 连接其他class
-    self.assets = Assets() # 资源文件
-    self.config = Config() # 配置文件
-    self.player = Player(self) # 玩家信息
-    self.clickEvent = ClickEvent(self)
-    self.roolBG = GameBackground(self) # 滚动背景
-    self.mainDraw = MainDraw(self) # 绘制
-    self.mainAction = MainAction(self) # 动作
+    self.assets = Assets()              # 资源文件
+    self.config = Config()              # 配置文件
+    # self.gsprite = GSprite(self)
+    self.player = Player(self)          # 玩家信息
+    self.clickEvent = ClickEvent(self)  # 点击事件
+    self.roolBG = GameBackground(self)  # 滚动背景
+    self.mainDraw = MainDraw(self)      # 绘制
+    self.mainAction = MainAction(self)  # 动作
 
 
 
@@ -273,6 +358,14 @@ class MainScene(object):
         sys.exit()
       if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
         self.clickEvent.LeftClick(event.pos)
+      if event.type == pygame.KEYDOWN and self.mainDraw.uipart == 2:
+        self.player.friction = 1
+        if event.key == pygame.K_SPACE:
+          self.player.Switch()
+        elif event.key == pygame.K_UP:
+          self.player.Jump()
+      elif event.type == pygame.KEYUP and self.mainDraw.uipart == 2:
+        self.player.friction = 0.99
     pass
 
   # 碰撞检测
--- core.py
+++ core.py
@@ -13,7 +13,7 @@ current_h: 当前显示模式或窗口的像素高度
 """
 size = width, height = (1600, 900)
 # size = width, height = 600, 400
-speed = [1, 1]
+speed = [0, 1]
 BLACK = 255, 255, 255
 # screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 # screen = pygame.display.set_mode(size, pygame.RESIZABLE)
@@ -21,7 +21,7 @@ screen = pygame.display.set_mode(size, 0, 32)
 pygame.display.set_caption("微光 | Glimmer - 0ojixueseno0")
 ball = pygame.image.load("assets/picture/wg-ball.png").convert_alpha()
 ballrect = ball.get_rect()
-ballrect = ballrect.move(400, 800)
+ballrect = ballrect.move(400, 600)
 fps = 120
 fclock = pygame.time.Clock()
 
@@ -34,12 +34,6 @@ while True:
     if event.type == pygame.QUIT:
       sys.exit()
     elif event.type == pygame.KEYDOWN:
-      # if event.key == pygame.K_LEFT:
-      #   speed[0] = speed[0] if speed[0] == 0 else (abs(speed[0]) - 1)*int(speed[0]/abs(speed[0]))
-      #   print(speed[0])
-      # elif event.key == pygame.K_RIGHT:
-      #   speed[0] = speed[0] + 1 if speed[0] > 0 else speed[0] - 1
-      #   print(speed[0])
       if event.key == pygame.K_SPACE:
         if onground:
           onground = False
@@ -49,15 +43,12 @@ while True:
           onground = False
           speed[1] = -32 * gravity
           print(speed[1])
-      # elif event.key == pygame.K_DOWN:
-      #   speed[1] = speed[1] if speed[1] == 0 else (abs(speed[1]) - 1)*int(speed[1]/abs(speed[1]))
-      #   print(speed[1])
       elif event.key == pygame.K_ESCAPE:
         sys.exit()
       friction = 1
     elif event.type == pygame.KEYUP:
       friction = 0.99
-    speed[0] = speed[0] * 0.92
+    # speed[0] = speed[0] * 0.92
     speed = [friction * s for s in speed]
 
   speed[1] += gravity
